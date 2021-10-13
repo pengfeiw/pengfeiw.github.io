2821511d858a9f83172e628d8e48b49e3403c7a0
pengfeiw <13671619364@sina.cn>
2021-10-13 06:09:37 +0000

    Deploying to gh-pages from @ pengfeiw/pengfeixc.com@8e18d8121c7d24582da880cb27fbe9a25cdf75e4 🚀

---
+++ _next/static/6mjtTACnHG5V_YX1UsVI3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/63876c1dd666ae620ee1.css","static/chunks/pages/index-e1ef50d60f29ce5d0473.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
