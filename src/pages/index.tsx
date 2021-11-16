b865a38b628ba6b3a9770b92a272d921ab567e06
wangpengfei <2601339907@qq.com>
2021-11-16 15:47:45 +0800

    feat: add business card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,16 +12,29 @@ const Home: NextPage = () => {
             </Head>
 
             <main className={styles.main}>
-                <p className={styles.starText}>
-                    ************     ***** 
-                    ************     *****   
-                        ****         *****
-                        ****         *****
-                        ****         *****   
-                        ****         *****   
-                    ************     **************   
-                    ************     **************   
-                </p>
+                <div className={styles.card}>
+                    <div className={styles.cardT}>
+                        <img className={styles.logo} src="/avatar.svg" alt="Wang Pengfei" />
+                        <div>
+                            <h1>Wang Pengfei</h1>
+                            <p className={styles.tag}>Interests.Learn.Practice.Success</p>
+                        </div>
+                    </div>
+                    <div className={styles.cardD}>
+                        <a href="https://github.com/pengfeiw" target="_blank">
+                            <img className={styles.icon} src="/github.svg" alt="https://github.com/pengfeiw" />
+                        </a>
+                        <a href="mailto:pengfeixc@sina.com" target="_blank">
+                            <img className={styles.icon} src="/email.svg" alt="mailto:pengfeixc@sina.com" />
+                        </a>
+                        <a href="https://pengfeixc.com" target="_blank">
+                            <img className={styles.icon} src="/site.svg" alt="https://pengfeixc.com" />
+                        </a>
+                        <a href="https://mini.pengfeixc.com" target="_blank">
+                            <img className={styles.icon} src="/m.svg" alt="https://mini.pengfeixc.com" />
+                        </a>
+                    </div>
+                </div>
             </main>
         </div>
     )
