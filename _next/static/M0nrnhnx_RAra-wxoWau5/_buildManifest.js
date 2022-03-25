6b2cb6c862acb4273fc2d8e4a74b30ccfec25f71
pengfeiw <pengfeixc@sina.com>
2022-03-25 04:13:05 +0000

    Deploying to gh-pages from @ pengfeiw/pengfeiw.github.io@cc163fce1eb127459c91146880d39d7d77e95895 🚀

--- _next/static/M0nrnhnx_RAra-wxoWau5/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ff56950aa23be7dc7ed1.css","static/chunks/pages/index-270b0ba94bb827b9ecb2.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
